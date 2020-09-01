8399c057d9a9000d95a7f696ac39ee1fef79dc6b
Andrew KIl <kil.andrew@gmail.com>
2020-09-01 19:07:46 -0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function App() {
-  return <div className="App">App</div>;
-}
+import { Column } from "./Column";
+import { Card } from "./Card";
+import { AddNewItem } from "./AddNewItem";
+
+import { AppContainer } from "./styles";
+
+const App = () => {
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
+};
 
 export default App;
